81e68beaf8533b8b4177618bc6550f1967758b01
lyron08 <49680499+lyron08@users.noreply.github.com>
2024-10-14 17:22:38 +0800

    Initial Upload

---
+++ classes/Database.php
@@ -0,0 +1,21 @@
+<?php
+
+class Database {
+    private $server_name = "localhost";
+    private $username = "root";
+    private $password = ""; // root - for MAMP user
+    private $db_name = "the_company_october";
+    protected $conn; // Child classes that extends to this class e.g Users.php is allowed to use
+    /**
+     * Only protected and public properties can child classes use
+     */
+    public function __construct(){
+        $this->conn = new mysqli($this->server_name, $this->username, $this->password, $this->db_name);
+
+        if($this->conn->connect_error){
+            die("Unable to connect to the database: " . $this->conn->connect_error);
+        }
+    }
+
+}
+?>
